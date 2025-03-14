42eee82c94c3cc2a88c38b1953d285a2cd4661f8
Your Name <you@example.com>
2025-03-14 15:13:23 +0300

    feat: add base constructor for 2D figures

---
+++ GraphicEditor/Shapes/Base2DFigure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace GraphicEditor.Shapes
+{
+    abstract class Base2DFigure : AShape
+    {
+        public Base2DFigure(Point lt, Point rb,
+                    Brush Stroke, double StrokeThickness, Brush Fill)
+                : base(lt, rb, Stroke, StrokeThickness, Fill)
+        {
+            (this.LeftTop.X, this.RightBottom.X) = lt.X > rb.X ? (rb.X, lt.X) : (this.LeftTop.X, this.RightBottom.X);
+            (this.LeftTop.Y, this.RightBottom.Y) = lt.Y > rb.Y ? (rb.Y, lt.Y) : (this.LeftTop.Y, this.RightBottom.Y);
+        }
+    }
+}
