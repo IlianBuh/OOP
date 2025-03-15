a5c81668c12e42268430b81257c2da5d83adbfe1
Your Name <you@example.com>
2025-03-15 10:19:58 +0300

    fix: correct setters in Base2DFigure

--- GraphicEditor/Shapes/Base2DFigure.cs
+++ GraphicEditor/Shapes/Base2DFigure.cs
@@ -10,15 +10,21 @@ namespace GraphicEditor.Shapes
 {
     abstract class Base2DFigure : AShape
     {
-        public double Height { get => Math.Abs(EndPoint.Y - Anchor.Y); set => EndPoint.Y = Anchor.Y + value; }
-        public double Width { get => Math.Abs(EndPoint.X - Anchor.X); set => EndPoint.X = Anchor.X + value; }
+        public double Height { 
+            get => Math.Abs(EndPoint.Y - Anchor.Y);
+            set => this.EndPoint = new (this.EndPoint.X, Anchor.Y + value);
+        }
+        public double Width { 
+            get => Math.Abs(EndPoint.X - Anchor.X); 
+            set => this.EndPoint = new (Anchor.X + value, this.EndPoint.Y); 
+        }
         public double Left { 
             get => this.Anchor.X > this.EndPoint.X ? this.EndPoint.X : this.Anchor.X;
-            set => this.EndPoint.X = value;
+            set => this.EndPoint = new(value, this.EndPoint.Y);
         }
         public double Top { 
             get => this.Anchor.Y > this.EndPoint.Y ? this.EndPoint.Y : this.Anchor.Y;
-            set => this.EndPoint.Y = value;
+            set => this.EndPoint = new(this.EndPoint.X, value);
         }
         public Base2DFigure(Point lt, Point rb,
                     Brush Stroke, double StrokeThickness, Brush Fill)
