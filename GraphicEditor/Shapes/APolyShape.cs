92dff33a863bb0343114e2ddb0cd4351a0fc68b9
Your Name <you@example.com>
2025-03-15 10:34:52 +0300

    feat: add an abstract class for shapes that are drawn by points

---
+++ GraphicEditor/Shapes/APolyShape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace GraphicEditor.Shapes
+{
+    abstract class APolyShape : AShape
+    {
+
+        public List<Point> Points = [];
+        public APolyShape(Point anch, Point endP,
+                        Brush Stroke = null, double StrokeThickness = 3, Brush Fill = null)
+                : base(anch, endP, Stroke, StrokeThickness, Fill)
+        {
+            this.Points = [anch, endP];
+        }
+        abstract public void AddPoint(Point point);
+    }
+}
