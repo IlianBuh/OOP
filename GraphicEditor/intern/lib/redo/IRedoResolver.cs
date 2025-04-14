9feb22f8b1c079bf408e29e9256a06a7890d9282
Your Name <you@example.com>
2025-04-14 13:32:38 +0300

    feat: add redo interface

---
+++ GraphicEditor/intern/lib/redo/IRedoResolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicEditor.intern.lib.stack
+{
+    interface IRedoResolver<T>
+    {
+        public (T shape, bool ok) GetShape();
+        public void AddShape(T shape);
+        public void Clear();
+
+    }
+}
